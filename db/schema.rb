3fa99cb70c226fe85a35c2fd13df91807a2c3525
sekimori0802 <t.s.0514.0952@gmail.com>
2019-10-06 20:08:08 +0900

    brandモデルの作成
    brandモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_105621) do
+ActiveRecord::Schema.define(version: 2019_10_06_110249) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
