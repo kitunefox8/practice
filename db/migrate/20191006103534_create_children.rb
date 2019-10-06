e12535f72ea7b3af254558faa308964cdee44155
sekimori0802 <t.s.0514.0952@gmail.com>
2019-10-06 19:41:47 +0900

    childrenモデルの作成
    children（categoryの子）モデルの作成

---
+++ db/migrate/20191006103534_create_children.rb
@@ -0,0 +1,8 @@
+class CreateChildren < ActiveRecord::Migration[5.2]
+  def change
+    create_table :children do |t|
+    t.string :name, null: false
+    t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_102603) do
+ActiveRecord::Schema.define(version: 2019_10_06_103534) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "children", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
