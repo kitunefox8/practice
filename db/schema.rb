01c53468103dad929a2d91781b2869ef8de472c1
sekimori0802 <t.s.0514.0952@gmail.com>
2019-10-06 19:31:25 +0900

    categoryモデルの作成＆imageモデルの修正
    categoryモデルの作成
    image_urlにnot_null制約を付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_102032) do
+ActiveRecord::Schema.define(version: 2019_10_06_102603) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "creditcards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "credit_namber", null: false
@@ -19,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_10_06_102032) do
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image_url"
+    t.string "image_url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
