071d6f02cf91734d279b87ba719b6f0e68dadd11
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-08 12:07:46 +0900

    仮置きのルーティング（再）
    ルーティング修正
    ログインしているかの確認を一時コメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "image_test#index"
+  root "image_tests#index"
   
-  resources :product, only: [:index]
-  resources :image_test, only: [:index, :create]
+  resources :products, only: [:index]
+  resources :image_tests, only: [:index, :create]
 
 end
