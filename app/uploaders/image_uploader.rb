b690423b0edab9aa487e42389b6c46f2398c585c
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-08 15:48:58 +0900

    画像保存先を環境ごとに分岐
    image_uploader.rbに条件分岐を追加
    s3への保存storage :fogは本番環境のみに

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,17 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [200, 200]
   
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+  
+  # 画像の保存先を環境ごとに分岐
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
