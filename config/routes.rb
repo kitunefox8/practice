c12252c5dc95a9106218b25353cb3540d48e3b19
Rita0702 <0702.t.kawakami@gmail.com>
2019-10-08 12:02:46 +0900

    仮置きのルーテイング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "products#index"
+  root "image_test#index"
   
-  resources :products, only: [:index]
+  resources :product, only: [:index]
+  resources :image_test, only: [:index, :create]
 
 end
